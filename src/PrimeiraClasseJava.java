6650b3f887ed4289f217b4108ef3b67ffa40c2c2
prPedr <prpedro025@gmail.com>
2026-09-07 16:07:01 -0300

    Primeiro commit

---
+++ src/PrimeiraClasseJava.java
@@ -0,0 +1,13 @@
+public class PrimeiraClasseJava {
+    public static void main(String[] args) {
+        int maiorIdade = 18;
+
+        int minhaIdade = 16;
+
+        if (minhaIdade >= maiorIdade) {
+            System.out.println("Voce é maior de idade.");
+        } else {
+            System.out.println("Voce é menor de idade.");
+        }
+    }
+}
