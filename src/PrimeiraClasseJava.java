eb3c883af7073cdcc7a8f513ef8311c57afb2252
prPedr <prpedro025@gmail.com>
2026-09-07 16:35:04 -0300

    Declarando variaves globais

--- src/PrimeiraClasseJava.java
+++ src/PrimeiraClasseJava.java
@@ -1,13 +1,27 @@
 public class PrimeiraClasseJava {
-    public static void main(String[] args) {
-        int maiorIdade = 18;
+    // Variavel global => Acessivel de qualquer local
+    static String meuNome = "Pedro";
 
-        int minhaIdade = 16;
+    public static void main(String[] args) {
+        nomeUsuario();
+        maiorIdade();
+    }
+
+    public static void nomeUsuario() {
+        System.out.println("Meu nome é " + meuNome);
+    }
+
+    public static void maiorIdade() {
+        int maiorIdade, minhaIdade;
+
+        // Variavel local => Acessivel somente dentro do seu metodo
+        maiorIdade = 18;
+        minhaIdade = 21;
 
         if (minhaIdade >= maiorIdade) {
-            System.out.println("Voce é maior de idade.");
+            System.out.println("E sou maior de idade");
         } else {
-            System.out.println("Voce é menor de idade.");
+            System.out.println("E sou menor de idade");
         }
     }
 }
